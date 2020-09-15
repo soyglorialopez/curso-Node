33700b2641f25f57fcdecd9c99a7a707a89f0c4c
[gloria] <glorialopez.gelm@gmail.com>
2020-09-14 23:54:50 -0400

    microservice-DB

---
+++ mysql/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const config = require('../config');
+const router = require('./network');
+const app = express();
+
+app.use(express.json());
+
+//Rutas
+app.use('/', router)
+
+app.listen(config.mysqlService.port, () => {
+    console.log(`Servicio de mysql escuchando en el puerto ${config.mysqlService.port}`)
+});
+
+
